84cb164631a6b115342b5c790927bc4c8ddc62ba
quinnzwarich <70957770+quinnzwarich@users.noreply.github.com>
2023-01-27 12:16:49 -0500

    made scripts that rotate objects

---
+++ Assets/rotator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour
+
+{
+
+    public float rotationSpeed = 0.3f;
+
+    private bool rotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R)) 
+        {
+            rotate = true;
+        }
+        else 
+        {
+            rotate = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (rotate)
+        {
+            this.GetComponent<Transform>().Rotate(0.3f, rotationSpeed, 0.2f);
+        }
+    }
+}
